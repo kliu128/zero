c9b5409efd8d582c645d56f59b64293fd5671d89
Kevin Liu <kevin@potatofrom.space>
2018-04-19 13:51:55 -0400

    Encrypt password hash

--- common/users.nix
+++ common/users.nix
@@ -3,14 +3,13 @@
 {
   # Disable adding/removing users on the fly
   users.mutableUsers = false;
-# Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.root.hashedPassword = "***REMOVED***";
+  users.users.root.hashedPassword = builtins.readFile ../secrets/password-hash.txt;
   users.extraUsers.kevin = {
     description = "Kevin Liu";
     isNormalUser = true;
     uid = 1000;
     extraGroups = [ "wheel" ];
-    hashedPassword = "***REMOVED***";
+    hashedPassword = builtins.readFile ../secrets/password-hash.txt;
   };
   security.sudo.enable = true;
 }
