d8867cf0a2a8c10863445ca4ac1d348394127b7f
Kevin Liu <kevin@potatofrom.space>
2018-05-03 23:09:03 -0400

    rem: install cointop

--- rem/desktop.nix
+++ rem/desktop.nix
@@ -56,7 +56,7 @@
   programs.wireshark.package = pkgs.wireshark-gtk;
   environment.systemPackages = with pkgs; [
     # System tools
-    i7z atop bcachefs-tools beets borgbackup cowsay cryptop ctop dnsutils file fortune docker_compose git-crypt gitAndTools.gitFull gnupg hdparm htop iftop iotop python python3 libva-full lm_sensors lolcat looking-glass-client lzip mpw nheko oh-my-zsh openjdk python36Packages.tvnamer rclone restic rustup screen smartmontools snapraid spectre-meltdown-checker stress-ng telnet tmux tree uptimed vim vnstat wget unar
+    i7z atop bcachefs-tools beets borgbackup cointop cowsay ctop dnsutils file fortune docker_compose git-crypt gitAndTools.gitFull gnupg hdparm htop iftop iotop python python3 libva-full lm_sensors lolcat looking-glass-client lzip mpw nheko oh-my-zsh openjdk python36Packages.tvnamer rclone restic rustup screen smartmontools snapraid spectre-meltdown-checker stress-ng telnet tmux tree uptimed vim vnstat wget unar
     # Desktop applications
     android-studio arduino atom calibre chromium clementine codeblocks discord electron-cash emacs gnome3.gedit gnome3.nautilus filezilla firefox gpodder hexchat jetbrains.idea-community jetbrains.pycharm-community libreoffice-fresh liferea mate.atril mkvtoolnix mpv multimc pavucontrol simple-scan slack spectacle thunderbird transmission_gtk transmission_remote_gtk vlc vscode wine winetricks youtube-dl
     # Development
