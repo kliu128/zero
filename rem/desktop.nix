b57a8871868a44d57e8c6a2c4f0f055a9c476c41
Kevin Liu <kevin@potatofrom.space>
2018-05-30 19:32:14 -0400

    rem: add skypeforlinux

--- rem/desktop.nix
+++ rem/desktop.nix
@@ -60,7 +60,7 @@
     # System tools
     i7z atop bcachefs-tools beets borgbackup cointop cowsay ctop dnsutils file fortune docker_compose  gnupg hdparm htop iftop iotop python python3 libva-full lm_sensors lolcat looking-glass-client lzip mpw nheko oh-my-zsh openjdk python36Packages.tvnamer rclone restic rustup screen smartmontools snapraid spectre-meltdown-checker stress-ng telnet tmux tree uptimed vim vnstat wget unar
     # Desktop applications
-    android-studio arduino atom calibre cantata chromium clementine codeblocks discord electron-cash emacs gnome3.gedit gnome3.gnome-disk-utility gnome3.nautilus filezilla firefox gpodder hexchat jetbrains.idea-community jetbrains.pycharm-community libreoffice-fresh liferea mate.atril mkvtoolnix mpv multimc pavucontrol simple-scan slack spectacle thunderbird transmission_gtk transmission_remote_gtk vlc vscode wine winetricks youtube-dl
+    android-studio arduino atom calibre cantata chromium clementine codeblocks discord electron-cash emacs gnome3.gedit gnome3.gnome-disk-utility gnome3.nautilus filezilla firefox gpodder hexchat jetbrains.idea-community jetbrains.pycharm-community libreoffice-fresh liferea mate.atril mkvtoolnix mpv multimc pavucontrol skypeforlinux simple-scan slack spectacle thunderbird transmission_gtk transmission_remote_gtk vlc vscode wine winetricks youtube-dl
     # Development
     bfg-repo-cleaner gcc gdb git-crypt gitAndTools.gitFull gnumake
     # Games
