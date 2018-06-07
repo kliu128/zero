1c81b26574e74a206c05d2b8b0b39f1e6d4a230b
Kevin Liu <kevin@potatofrom.space>
2018-06-07 14:50:01 -0400

    docker: mark as after remote-fs.target
    To prevent NFS mounts from being unmounted before stopping Docker.

--- modules/docker.nix
+++ modules/docker.nix
@@ -10,6 +10,8 @@
   systemd.services.docker.preStop = ''
     ${pkgs.docker}/bin/docker stop $(${pkgs.docker}/bin/docker ps -q)
   '';
+  systemd.services.docker.after = [ "remote-fs.target" ];
+  systemd.services.docker.wants = [ "remote-fs.target" ];
   users.extraUsers.kevin.extraGroups = [ "docker" ];
   networking.firewall.allowedTCPPorts = [ 2376 2377 7946 ];
   networking.firewall.allowedUDPPorts = [ 4789 7946 ];
