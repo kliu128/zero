f0d736daa86e20f9b7ed845aa1c1c4ed24083af1
Kevin Liu <kevin@potatofrom.space>
2018-06-08 18:57:29 -0400

    kubernetes: remove custom snapshot configuration from etcd

--- modules/kubernetes-master.nix
+++ modules/kubernetes-master.nix
@@ -6,11 +6,6 @@
 
   services.etcd = {
     enable = true;
-    extraConf = {
-      # Default is 10k, lowers etcd memory usage by having less in-memory
-      # snapshots
-      SNAPSHOT_COUNT = "2500";
-    };
     listenClientUrls = [ "https://0.0.0.0:2379" ];
     advertiseClientUrls = [ "https://192.168.1.5:2379" ];
 
