7de695e97c122341788e5f9cf77e6c4f87836db4
Kevin Liu <kevin@potatofrom.space>
2018-07-10 18:27:19 -0400

    kernel: use 4.14 with PREEMPT patch

--- nixops/common/kernel.nix
+++ nixops/common/kernel.nix
@@ -1,5 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  nixpkgs.config.packageOverrides = pkgs: rec {
+    linux_4_14 = pkgs.linux_4_14.override {
+      # Reduce latency, please
+      extraConfig = ''
+        PREEMPT y
+        PREEMPT_RCU y
+      '';
+    };
+  };
+  boot.kernelPackages = pkgs.linuxPackages_4_14;
 }
