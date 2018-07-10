629b9a92e40ba1a4e20ce3a2ade34e22be4a37b8
Kevin Liu <kevin@potatofrom.space>
2018-07-10 19:52:29 -0400

    docker: fix flannel breaking on subnet change

--- nixops/modules/docker.nix
+++ nixops/modules/docker.nix
@@ -3,9 +3,17 @@
 {
   virtualisation.docker.enable = true;
   virtualisation.docker.autoPrune.enable = true;
+  # Enabling live restore breaks flannel -- see a similar note at
+  # https://github.com/kubernetes-incubator/kubespray/blob/a39e78d42d5bcb6893d0981fc478a3883364fdae/roles/network_plugin/flannel/handlers/main.yml#L12
+  # When flannel changes its subnet and changes `/run/flannel/docker`, thus
+  # changing Docker's `--bip` argument, Docker appears to ignore it when
+  # liveRestore is enabled, and keeps using the old IP for `docker0`. So, uh,
+  # don't do that.
+  virtualisation.docker.liveRestore = false;
   systemd.services.docker = {
     after = [ "remote-fs.target" ];
     wants = [ "remote-fs.target" ];
+    restartIfChanged = false;
   };
   systemd.services.docker-shutdown = {
     wantedBy = [ "multi-user.target" ];
