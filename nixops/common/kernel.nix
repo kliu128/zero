85cd35fc7608b0f3281ec11fd257dc75dc4a06ae
Kevin Liu <kevin@potatofrom.space>
2018-07-12 13:49:22 -0400

    kernel: remove PREEMPT config
    Appears to cause stutters/lag on high disk i/o.

--- nixops/common/kernel.nix
+++ nixops/common/kernel.nix
@@ -1,14 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.config.packageOverrides = pkgs: rec {
-    linux_4_14 = pkgs.linux_4_14.override {
-      # Reduce latency, please
-      extraConfig = ''
-        PREEMPT y
-        PREEMPT_RCU y
-      '';
-    };
-  };
-  boot.kernelPackages = pkgs.linuxPackages_4_14;
+  boot.kernelPackages = pkgs.linuxPackages;
 }
