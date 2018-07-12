2b004b2239d040e0b3491705085bb19863b0ae32
Kevin Liu <kevin@potatofrom.space>
2018-07-12 15:09:16 -0400

    rem: add network logger for OpenWRT router

--- nixops/rem/monitoring.nix
+++ nixops/rem/monitoring.nix
@@ -1,7 +1,24 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Self-monitoring
   services.vnstat.enable = true;
   services.uptimed.enable = true;
   environment.systemPackages = [ pkgs.vnstat pkgs.uptimed ];
+
+  # Monitoring of OpenWRT router through network logging (UDP port 514)
+  networking.firewall.allowedUDPPorts = [ 514 ];
+  services.rsyslogd = {
+    enable = true;
+    extraConfig = ''
+      module(load="imudp")
+      module(load="omjournal") 
+
+      ruleset(name="remote") {
+        action(type="omjournal")
+      }
+
+      input(type="imudp" port="514" ruleset="remote")
+    '';
+  };
 }
