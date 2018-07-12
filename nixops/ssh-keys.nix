7a08e797bc0f331533d929f5e3334f8968b8a115
Kevin Liu <kevin@potatofrom.space>
2018-07-12 13:50:34 -0400

    ssh-keys: add ilo2 sandbox vm ssh key for reference

--- nixops/ssh-keys.nix
+++ nixops/ssh-keys.nix
@@ -1,5 +1,6 @@
 {
   kevin-rem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQDtQm7AWGKLgSK2TE1nIus65ZD+jQl6TVGHQaOfFn/ kevin@rem";
+  kevin-rem-vm-ilo2-sandbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGB8DdZut/WsKtOEwyeheRNM5WJUSahKucm6IkrmbUSW kevin@rem-vm-ilo2-sandbox";
   kevin-emilia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvRm/l9jaQ3fN5ZQvmZCfhKGHgtizonT9BRSKFbbgro kevin@emilia";
   kevin-xt1575 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGBlgyWrDlIJ5RWigaWGOmjVWBpPqqJ/cL58yJblfm33 kevin@xt1575";
 }
