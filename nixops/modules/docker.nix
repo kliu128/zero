f4a8724b75d78cb1051f49a469b9acc9ae9d684a
Kevin Liu <kevin@potatofrom.space>
2018-07-23 19:36:36 -0400

    docker: remove docker-shutdown unit

--- nixops/modules/docker.nix
+++ nixops/modules/docker.nix
@@ -15,17 +15,6 @@
     wants = [ "remote-fs.target" ];
     restartIfChanged = false;
   };
-  systemd.services.docker-shutdown = {
-    wantedBy = [ "multi-user.target" ];
-    restartIfChanged = false;
-    serviceConfig.Type = "oneshot";
-    serviceConfig.RemainAfterExit = true;
-    after = [ "docker.service" "docker.socket" "local-fs.target" "nfs-server.service" "remote-fs.target" ];
-    before = [ "kubelet.service" ];
-    preStop = ''
-      ${pkgs.docker}/bin/docker stop $(${pkgs.docker}/bin/docker ps -q)
-    '';
-  };
   users.extraUsers.kevin.extraGroups = [ "docker" ];
   networking.firewall.allowedTCPPorts = [ 2376 2377 7946 ];
   networking.firewall.allowedUDPPorts = [ 4789 7946 ];
