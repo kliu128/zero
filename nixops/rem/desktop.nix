ecee0dea90dcaea238b1f3cc9b3b9c5d9f0152ad
Kevin Liu <kevin@potatofrom.space>
2018-08-03 09:02:47 -0400

    Remove electron-cash (build broken on nixpkgs master)

--- nixops/rem/desktop.nix
+++ nixops/rem/desktop.nix
@@ -3,10 +3,10 @@
 {
   # rem-specific desktop configuration
   home-manager.users.kevin.xsession.windowManager.i3.extraConfig = ''
-    workspace 1 output HDMI-1
-    workspace 2 output DP-1
-    workspace 3 output DVI-D-1
-    exec xrandr --output DVI-D-1 --mode 1280x1024 --pos 3840x56 --rotate normal --output HDMI-1 --mode 2560x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --mode 1280x1024 --pos 2560x56 --rotate normal --output HDMI-2 --off
+    workspace 1 output HDMI-A-0
+    workspace 2 output DisplayPort-0
+    workspace 3 output DVI-D-0
+    exec xrandr --output DVI-D-0 --mode 1280x1024 --pos 3840x56 --rotate normal --output HDMI-A-0 --mode 2560x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --mode 1280x1024 --pos 2560x56 --rotate normal
     
     # STARTUP APPLICATIONS
     # ====================
