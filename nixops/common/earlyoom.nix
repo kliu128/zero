79ed354be77ffc504d7683ce5afa1aaef2099692
Kevin Liu <kevin@potatofrom.space>
2018-08-05 10:18:53 -0400

    earlyoom: lower freeMemThreshold to 5%

--- nixops/common/earlyoom.nix
+++ nixops/common/earlyoom.nix
@@ -2,6 +2,7 @@
 
 {
   services.earlyoom.enable = true;
+  services.earlyoom.freeMemThreshold = 5; # ~1 GB for rem, or 400 MB for puck
 
   # Occasionally crashes with the message:
   # > Could not convert number: Numerical result out of range
