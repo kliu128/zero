7807ba334177e9e28d1e86b819d973b1b9638e61
Kevin Liu <kevin@potatofrom.space>
2018-08-07 08:37:52 -0400

    desktop: switch from pcmanfm to dolphin

--- nixops/modules/desktop.nix
+++ nixops/modules/desktop.nix
@@ -283,7 +283,7 @@ with lib;
       # VM and DevOps
       helmfile kubectl kubernetes-helm nixops virtmanager
       # Desktop environment
-      arandr blueman conky i3lock nitrogen pcmanfm system-config-printer scrot xautolock xcape xcompmgr termite
+      arandr blueman conky dolphin i3lock nitrogen system-config-printer scrot xautolock xcape xcompmgr termite
     ];
     pam.sessionVariables = {
       GPODDER_HOME = "/home/kevin/.config/gPodder";
