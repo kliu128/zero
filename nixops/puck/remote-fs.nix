38d6e6b7836632246ba50390712dd68aecbe367d
Kevin Liu <kevin@potatofrom.space>
2018-09-07 18:29:00 -0400

    Move to LizardFS + NFS

--- nixops/puck/remote-fs.nix
+++ nixops/puck/remote-fs.nix
@@ -1,26 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  systemd.services.storage = {
-    enable = true;
-    description = "Grand Stores Mount";
-    path = [ pkgs.lizardfs ];
-    restartIfChanged = false; # don't want the filesystem falling out from under processes
-    script = ''
-      mfsmount -o nodev,noatime,mfsdelayedinit,big_writes,allow_other,nonempty,mfsmaster=10.100.0.1 /mnt/storage
-    '';
-    wantedBy = [ "local-fs.target" ];
-    serviceConfig = {
-      Type = "forking";
-    };
-    unitConfig = {
-      # Implicitly adds dependency on basic.target otherwise, which creates
-      # an ordering cycle on boot
-      DefaultDependencies = false;
-      # Normally would be added by DefaultDependencies=
-      Conflicts = [ "shutdown.target" ];
-      Before = [ "shutdown.target" ];
-    };
+  fileSystems."/mnt/storage" = {
+    fsType = "nfs";
+    device = "${(import ../wireguard.nix).ips.rem}:/storage";
+    options = [ "x-systemd.automount" "noauto" "soft" "intr" ];
   };
   fileSystems."/mnt/home" = {
     fsType = "nfs";
