2db32e77c877ea8072eb6f621377aa2c135d3a69
Kevin Liu <kevin@potatofrom.space>
2018-09-20 15:42:19 -0400

    puck: auto mount/unmount NFS shares on net change

--- nixops/puck/remote-fs.nix
+++ nixops/puck/remote-fs.nix
@@ -1,14 +1,29 @@
 { config, pkgs, ... }:
 
 {
+  networking.networkmanager.dispatcherScripts = [ {
+    source = ./remote-fs.sh;
+    type = "basic";
+  } ];
   fileSystems."/mnt/storage" = {
     fsType = "nfs";
     device = "${(import ../wireguard.nix).ips.rem}:/storage";
-    options = [ "x-systemd.automount" "noauto" "soft" "intr" ];
+    options = [ "nofail" "noauto" "soft" "intr" ];
   };
   fileSystems."/mnt/home" = {
     fsType = "nfs";
     device = "${(import ../wireguard.nix).ips.rem}:/home";
-    options = [ "x-systemd.automount" "noauto" "soft" "intr" ];
+    options = [ "nofail" "noauto" "soft" "intr" ];
+  };
+  systemd.services.unmount-nfs = {
+    enable = true;
+    serviceConfig = {
+      ExecStop = "${pkgs.utillinux}/bin/umount -l -a -t nfs,nfs4";
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network-manager.service" "wpa_supplicant.service" ];
+    after = [ "network-manager.service" "wpa_supplicant.service" ];
   };
 }
