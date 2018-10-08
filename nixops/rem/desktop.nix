08c299eaca0ab89cd8b43cb54c4c47b568e95d1d
Kevin Liu <kevin@potatofrom.space>
2018-10-08 13:11:25 -0400

    Tweak desktop config

--- nixops/rem/desktop.nix
+++ nixops/rem/desktop.nix
@@ -5,4 +5,15 @@
   home-manager.users.kevin = {
     home.file.".config/sway/config".text = builtins.readFile ./sway;
   };
+
+  home-manager.users.kevin.xsession.windowManager.i3.extraConfig = ''
+    workspace 1 output HDMI-A-1
+    workspace 2 output DisplayPort-0
+    workspace 3 output DVI-D-0
+    exec xrandr --output DVI-D-0 --mode 1280x1024 --pos 3840x56 --rotate normal --output HDMI-A-1 --mode 2560x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --mode 1280x1024 --pos 2560x56 --rotate normal --output HDMI-A-0 --off
+    
+    # STARTUP APPLICATIONS
+    # ====================
+    exec --no-startup-id i3-msg 'workspace 1; exec firefox; workspace 3; layout tabbed; exec $term; emacsclient -c; workspace 2; layout tabbed; exec thunderbird; exec Discord'
+  '';
 }
--- nixops/rem/hw.nix
+++ nixops/rem/hw.nix
@@ -217,7 +217,7 @@
   } ];
 
   # IO scheduler
-  boot.kernelParams = [ "scsi_mod.use_blk_mq=N" "amdgpu.dc=0" "iommu=pt" ];
+  boot.kernelParams = [ "scsi_mod.use_blk_mq=N" "iommu=pt" ];
 
   # HACKS
 
