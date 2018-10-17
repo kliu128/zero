145e3e1e78c64522f08011d20d82fa9bdf68f854
Kevin Liu <kevin@potatofrom.space>
2018-10-17 19:52:45 -0400

    Add boinc amd, parity, fix boinc

--- nixops/modules/boinc.nix
+++ nixops/modules/boinc.nix
@@ -8,7 +8,7 @@
       cd /var/lib/boinc
       ${pkgs.boinc}/bin/boinccmd --set_run_mode always
       ${pkgs.boinc}/bin/boinccmd --set_network_mode always
-      ${pkgs.boinc}/bin/boinccmd --join_acct_mgr http://bam.boincstats.com/ Pneumaticat '${builtins.readFile ../secrets/boincstats-password.txt}'
+      ${pkgs.boinc}/bin/boinccmd --join_acct_mgr https://bam.boincstats.com/ Pneumaticat '${builtins.readFile ../secrets/boincstats-password.txt}'
     '';
     deps = [];
   };
