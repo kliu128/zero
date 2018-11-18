162fe97542b2597ebf5379877c9a3b301157df45
Kevin Liu <kevin@potatofrom.space>
2018-11-18 10:51:34 -0500

    Move Nix optimization options to common/

--- nixops/common/nix.nix
+++ nixops/common/nix.nix
@@ -1,5 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  # Configure NixOS/nix
+  nix.autoOptimiseStore = true;
   nixpkgs.config.allowUnfree = true;
+  nix.buildCores = 0; # use all available CPU cores
+  nix.daemonNiceLevel = 19;
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 1d";
 }
--- nixops/rem/nix.nix
+++ nixops/rem/nix.nix
@@ -1,13 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Configure NixOS/nix
-  nix.autoOptimiseStore = true;
   nix.nixPath = [ "nixpkgs=/etc/nixos/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" ];
-  nix.buildCores = 0; # use all available CPU cores
   nix.maxJobs = lib.mkDefault 8;
-  nix.daemonNiceLevel = 19;
-  nix.gc.automatic = true;
-  nix.gc.options = "--delete-older-than 1d";
-  system.copySystemConfiguration = true; # to /run/current-system/configuration.nix
 }
