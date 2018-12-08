53660aed4c7b78ba21daf89643fe5be24e4db6b4
Kevin Liu <kevin@potatofrom.space>
2018-12-07 19:31:23 -0500

    Change hw config

--- nixops/common/io-scheduler.nix
+++ nixops/common/io-scheduler.nix
@@ -1,8 +1,5 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.kernelParams = [ "scsi_mod.use_blk_mq=Y" ];
-  services.udev.extraRules = ''
-    ACTION=="add|change", KERNEL=="sd*[!0-9]|sr*|nvme0n*", ATTR{queue/scheduler}="mq-deadline"
-  '';
+  boot.kernelParams = [ "scsi_mod.use_blk_mq=N" ];
 }
