ff149372d54e963d91279447b3497bd8c5f06c63
Kevin Liu <kevin@potatofrom.space>
2018-12-30 20:50:28 -0500

    rem: enable blu-ray support

--- nixops/rem/bluray.nix
+++ nixops/rem/bluray.nix
@@ -2,6 +2,15 @@
 
 {
   # Blu-ray/disk system tools
-  environment.systemPackages = with pkgs; [ k3b makemkv dvdisaster rubyripper flac ];
+  environment.systemPackages = with pkgs; [
+    # Ripping
+    k3b makemkv dvdisaster rubyripper flac
+  ];
+
   boot.kernelModules = [ "sg" ]; # for MakeMKV
+
+  # Enable blu-ray support in vlc/etc.
+  nixpkgs.config.packageOverrides = pkgs: rec {
+    libbluray = pkgs.libbluray.override { withAACS = true; };
+  };
 }
