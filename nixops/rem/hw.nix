381058dd9d73c1084886bb1cb7f0deafc47444a4
Kevin Liu <kevin@potatofrom.space>
2019-01-03 18:14:12 -0500

    rem: reenable logging to disk

--- nixops/rem/hw.nix
+++ nixops/rem/hw.nix
@@ -202,7 +202,6 @@
   services.fstrim.enable = true;
   boot.cleanTmpDir = true;
   boot.tmpOnTmpfs = true;
-  services.journald.extraConfig = "Storage=volatile";
   boot.kernel.sysctl."vm.min_free_kbytes" = 512000;
   boot.kernel.sysctl."vm.swappiness" = 10;
   swapDevices = [ {
