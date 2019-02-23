a01d96160e87d96041ff5a0f56d2408ba2f96a34
Kevin Liu <kevin@potatofrom.space>
2019-02-23 13:14:30 -0500

    Switch back to direct rclone backup

--- nixops/rem/backups.nix
+++ nixops/rem/backups.nix
@@ -53,37 +53,37 @@ in {
 
       # Backup with monthly backup dirs and revisioned suffixes in each
 
-      # dir_format="+%Y-%m"
-      # monthly_backup_dir="$(env TZ=Etc/UTC date "$dir_format")"
-      # backup_suffix="$(env TZ=Etc/UTC date +"__%Y_%m_%d_%H%M%SZ")"
-      # # Prune the backup dir from 6 months ago
-      # prune_backup_dir="$(env TZ=Etc/UTC date --date="-6 months" "$dir_format")"
+      dir_format="+%Y-%m"
+      monthly_backup_dir="$(env TZ=Etc/UTC date "$dir_format")"
+      backup_suffix="$(env TZ=Etc/UTC date +"__%Y_%m_%d_%H%M%SZ")"
+      # Prune the backup dir from 6 months ago
+      prune_backup_dir="$(env TZ=Etc/UTC date --date="-6 months" "$dir_format")"
 
       ${proxyConfig}
 
-      env RESTIC_PASSWORD_FILE=${../secrets/gsuite-backup-password.txt} \
-        XDG_CACHE_HOME=/var/cache/gsuite-backup \
-        RCLONE_CONFIG=/keys/rclone.conf \
-        restic --option=rclone.connections=16 \
-               --option=rclone.args='serve restic --stdio -v --drive-use-trash=false' \
-               --repo=rclone:gsuite-mysmccd:restic \
-               --verbose \
-               backup /mnt/storage/Kevin \
-               --exclude '/mnt/storage/Kevin/Incoming/**/*' 2>&1 \
-        | grep -v "can not obtain extended attribute system.richacl"
+      # env RESTIC_PASSWORD_FILE=${../secrets/gsuite-backup-password.txt} \
+      #   XDG_CACHE_HOME=/var/cache/gsuite-backup \
+      #   RCLONE_CONFIG=/keys/rclone.conf \
+      #   restic --option=rclone.connections=16 \
+      #          --option=rclone.args='serve restic --stdio -v --drive-use-trash=false' \
+      #          --repo=rclone:gsuite-mysmccd:restic \
+      #          --verbose \
+      #          backup /mnt/storage/Kevin \
+      #          --exclude '/mnt/storage/Kevin/Incoming/**/*' 2>&1 \
+      #   | grep -v "can not obtain extended attribute system.richacl"
 
-      # rclone --config /keys/rclone.conf \
-      #        sync /mnt/storage/Kevin gsuite-mysmccd-crypt:/Data/current \
-      #        --backup-dir "gsuite-mysmccd-crypt:/Data/old/$monthly_backup_dir" \
-      #        --bwlimit 8650k --transfers 32 \
-      #        --suffix "$backup_suffix" \
-      #        --exclude '/Computing/Data/**' \
-      #        --exclude '/Incoming/**' \
-      #        --exclude 'node_modules/**' \
-      #        --exclude '.fuse_hidden*' \
-      #        --delete-excluded -v --modify-window=1s --delete-during
-      # rclone --config /keys/rclone.conf \
-      #        purge -v "gsuite-mysmccd-crypt:/Data/old/$prune_backup_dir" || true
+      rclone --config /keys/rclone.conf \
+             sync /mnt/storage/Kevin gsuite-mysmccd-crypt:/Data/current \
+             --backup-dir "gsuite-mysmccd-crypt:/Data/old/$monthly_backup_dir" \
+             --bwlimit 8650k --transfers 32 \
+             --suffix "$backup_suffix" \
+             --exclude '/Computing/Data/**' \
+             --exclude '/Incoming/**' \
+             --exclude 'node_modules/**' \
+             --exclude '.fuse_hidden*' \
+             --delete-excluded -v --modify-window=1s --delete-during
+      rclone --config /keys/rclone.conf \
+             purge -v "gsuite-mysmccd-crypt:/Data/old/$prune_backup_dir" || true
     '';
     startAt = wave-3;
   };
