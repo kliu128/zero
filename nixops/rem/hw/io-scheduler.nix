930b983afa18f26d8178c12d7b87894657bfe507
Kevin Liu <kevin@potatofrom.space>
2019-03-28 21:26:46 -0400

    Change io scheduler

--- nixops/rem/hw/io-scheduler.nix
+++ nixops/rem/hw/io-scheduler.nix
@@ -2,11 +2,7 @@
 
 {
   boot.kernelParams = [ "scsi_mod.use_blk_mq=Y" ];
-  boot.kernel.sysctl = {
-    "vm.dirty_ratio" = 2;
-    "vm.dirty_background_ratio" = 1;
-  };
-  # services.udev.extraRules = ''
-  #   ACTION=="add|change", KERNEL=="sd*[!0-9]|sr*|nvme0n*", ATTR{queue/scheduler}="kyber"
-  # '';
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="sd*[!0-9]|sr*|nvme0n*", ATTR{queue/scheduler}="kyber"
+  '';
 }
