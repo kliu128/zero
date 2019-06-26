2264aad25984a81e942c319196147a815bd66ca7
Kevin Liu <kevin@potatofrom.space>
2019-06-26 15:32:40 -0400

    Switch matrix-discord back to Kubernetes

--- nixops/rem/services.nix
+++ nixops/rem/services.nix
@@ -1,21 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  systemd.services.matrix-discord = {
-    enable = true;
-    path = with pkgs; [ nix ];
-    script = ''
-      cd /home/kevin/Projects/matrix-appservice-discord
-      nix-shell -I nixpkgs=/etc/nixos/nixpkgs -p sqlite gcc python2 nodePackages.npm nodejs-8_x --run "npm start"
-    '';
-    serviceConfig = {
-      User = "kevin";
-      Group = "users";
-    };
-    wants = [ "network-online.target" ];
-    after = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-  };
   networking.firewall.allowedTCPPorts = [ 8080 9005 ];
   users.users.aneesh = {
     uid = 1007;
