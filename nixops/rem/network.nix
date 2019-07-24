2ca03f9f89a1a83345ee84d9df63a3a27fd16a02
Kevin Liu <kevin@potatofrom.space>
2019-07-23 23:27:19 -0400

    Add bridge

--- nixops/rem/network.nix
+++ nixops/rem/network.nix
@@ -8,11 +8,29 @@
   systemd.network.networks = {
     "40-eth0" = {
       matchConfig = { Name = "eth0"; };
-      DHCP = "yes";
+      networkConfig = {
+        Bridge = "br0";
+      };
+    };
+    "40-br0" = {
+      matchConfig = { Name = "br0"; };
+      networkConfig = {
+        DHCP = "ipv4";
+      };
+      linkConfig = {
+        MACAddress = "74:d4:35:e2:52:9b";
+      };
     };
     "99-main" = {
       matchConfig = { Name = "*"; };
       linkConfig = { Unmanaged = true; };
+    };
+  };
+  systemd.network.netdevs."br0" = {
+    enable = true;
+    netdevConfig = {
+      Name = "br0";
+      Kind = "bridge";
     };
   };
 
