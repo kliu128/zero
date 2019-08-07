ac2713b613c3cede9a520759bd6df6093a57bbe6
Kevin Liu <kevin@potatofrom.space>
2019-08-07 13:48:00 -0400

    Add hack3bot

--- nixops/rem/services.nix
+++ nixops/rem/services.nix
@@ -10,4 +10,23 @@
       cert = ../secrets/stunnel.pem;
     };
   };
+
+  systemd.services.hack3bot = {
+    enable = true;
+    description = "Hack3 Bot";
+    path = [ pkgs.yarn ];
+    script = ''
+      export BOT_TOKEN=$(cat /keys/hack3bot.token)
+      cd /home/kevin/Projects/hack3bot
+      yarn start
+    '';
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+  };
+  deployment.keys."hack3bot.token" = {
+    permissions = "600"; # rclone must be able to modify
+    destDir = "/keys";
+    text = builtins.readFile ../secrets/hack3bot.token;
+  };
 }
