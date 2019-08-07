883dd85cd08364c9164b962804f194ee98bc8177
Kevin Liu <kevin@potatofrom.space>
2019-08-07 13:48:13 -0400

    Fix gnome slow startup

--- nixops/modules/desktop/flatpak.nix
+++ nixops/modules/desktop/flatpak.nix
@@ -2,4 +2,6 @@
 
 {
   services.flatpak.enable = true;
+  systemd.user.services.xdg-desktop-portal.enable = lib.mkForce false;
+  systemd.user.services.xdg-desktop-portal-gtk.enable = lib.mkForce false;
 }
