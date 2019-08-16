a8338d97f823d80a59e272f8ece18254cbff8b23
Kevin Liu <kevin@potatofrom.space>
2019-08-16 17:26:47 -0400

    Revert to 4.19 kernel due to issues

--- nixops/systems/rem/hw/default.nix
+++ nixops/systems/rem/hw/default.nix
@@ -22,7 +22,7 @@
   
   # Video.
   boot.earlyVconsoleSetup = true;
-  services.xserver.videoDrivers = [ "modesetting" "amdgpu" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
   boot.kernelParams = [ "consoleblank=300" ];
 
   # Freeness (that is, not.)
