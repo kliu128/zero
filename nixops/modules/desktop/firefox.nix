4dfc610bccfb1713276682797fb49c86f89e0a6c
Kevin Liu <kevin@potatofrom.space>
2019-10-17 15:47:42 -0400

    Fix firefox scrolling?

--- nixops/modules/desktop/firefox.nix
+++ nixops/modules/desktop/firefox.nix
@@ -13,8 +13,5 @@
   environment.variables = {
     # Trick firefox so it doesn't create new profiles, see https://github.com/mozilla/nixpkgs-mozilla/issues/163
     SNAP_NAME = "firefox";
-    # Enable touchscreen support for Firefox
-    # see https://support.mozilla.org/en-US/questions/1091627
-    MOZ_USE_XINPUT2 = "1";
   };
 }
